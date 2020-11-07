c3539ac09210b79dd4619f5a8c7e68d508c96553
mr-iulian-rotaru <iulian@gnotis.ro>
2020-11-07 09:32:41 +0200

    Persist string on EEPROM, address 0.

--- src/E2PROM.h
+++ src/E2PROM.h
@@ -1,9 +1,11 @@
 #ifndef __HERA_E2PROM
 #define __HERA_E2PROM
 
+#include <Arduino.h>
 #include <EEPROM.h>
 
 #define EEPROM_BUFFER_SIZE 32
+#define EEPROM_STR_BUFFER_SIZE 100
 #define NO_EEPROM (byte)255
 
 class E2PROM {
@@ -18,6 +20,29 @@ class E2PROM {
     static void write(unsigned char addr, unsigned char value) {
       EEPROM.begin(EEPROM_BUFFER_SIZE);
       EEPROM.write(addr, value);
+      EEPROM.end();
+    }
+
+    static void read(String& string) {
+      EEPROM.begin(EEPROM_STR_BUFFER_SIZE);
+      for (unsigned char addr = 0; addr < EEPROM_STR_BUFFER_SIZE; ++addr) {
+        char c = EEPROM.read(addr);
+        if (c == 0) {
+          break;
+        }
+        string += c;
+      }
+      EEPROM.end();
+    }
+
+    static void write(const String& string) {
+      EEPROM.begin(EEPROM_STR_BUFFER_SIZE);
+      unsigned char addr = 0;
+      unsigned char length = min(string.length(), ((unsigned int)EEPROM_STR_BUFFER_SIZE - 1));
+      for (; addr < length; ++addr) {
+        EEPROM.write(addr, string.charAt(addr));
+      }
+      EEPROM.write(addr, 0);
       EEPROM.end();
     }
 
