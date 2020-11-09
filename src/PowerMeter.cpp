afda381b50d83e8bcb1dc07b9bf7857d251af1f8
mr-iulian-rotaru <iulian@gnotis.ro>
2020-11-09 22:39:06 +0200

    Add power meter to HERA library.

---
+++ src/PowerMeter.cpp
@@ -0,0 +1,63 @@
+#include "MessagePublisher.h"
+#include "PowerMeter.h"
+#include "Log.h"
+
+const char* PowerMeter::deviceClass = "js.hera.dev.PowerMeter";
+
+PowerMeter* powerMeter;
+
+ICACHE_RAM_ATTR void onPulseInterruptHandler() {
+  static unsigned long last_interrupt_time = 0;
+
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 100) {
+    powerMeter->onPulse();
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+PowerMeter::PowerMeter(const char* deviceName, const byte pin, const int ppwh):
+  Device(deviceClass, deviceName),
+  pin(pin),
+  ppwh(ppwh)
+{
+  pulseCount = 0;
+  pulseTime = 0;
+  lastTime = 0;
+}
+
+void PowerMeter::setup()
+{
+  Log::trace("PowerMeter::setup");
+  powerMeter = this;
+  pinMode(pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pin), onPulseInterruptHandler, RISING);
+}
+
+void PowerMeter::loop()
+{
+  if (power != 0) {
+    MessagePublisher::publishDeviceState(deviceName, power);
+    power = 0;
+  }
+}
+
+String PowerMeter::invoke(const String& action, const String& parameter) {
+  Log::trace("PowerMeter::invoke");
+
+  if (action == "test") {
+    return "OK";
+  }
+
+  return "";
+}
+
+void PowerMeter::onPulse() {
+  lastTime = pulseTime;
+  pulseTime = micros();
+  power = (3600000000.0 / (pulseTime - lastTime)) / ppwh;
+
+  pulseCount++;
+  energy = (1.0 * pulseCount / (ppwh * 1000));
+}
+
---
+++ src/PowerMeter.h
@@ -0,0 +1,32 @@
+#ifndef __HERA_POWER_METER
+#define __HERA_POWER_METER
+
+#include "Device.h"
+
+class PowerMeter : public Device {
+  public:
+    PowerMeter(const char* deviceName, const byte pin, const int ppwh=1);
+
+    void setup();
+    void loop();
+    String invoke(const String& action, const String& parameter = "");
+
+  public:
+    void onPulse();
+
+  private:
+    const byte pin;
+    const int ppwh;
+    unsigned long pulseCount;
+    unsigned long pulseTime;
+    unsigned long lastTime;
+
+    volatile double power;
+    volatile double energy;
+
+  private:
+    static const char* deviceClass;
+};
+
+#endif
+
