b3af336e2023b02c425406b15609480777442098
mr-iulian-rotaru <iulian@gnotis.ro>
2020-11-26 20:33:28 +0200

    Fix EEPROM buffer size specific to ESP.

--- src/E2PROM.h
+++ src/E2PROM.h
@@ -4,41 +4,44 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
-#define EEPROM_BUFFER_SIZE 32
-#define EEPROM_STR_BUFFER_SIZE 100
-#define EEPROM_ADDR_OFFSET EEPROM_STR_BUFFER_SIZE + 1
+#define EEPROM_BUFFER_SIZE (byte)255
+#define EEPROM_STR_BUFFER_SIZE (byte)100
+#define EEPROM_ADDR_OFFSET EEPROM_STR_BUFFER_SIZE + (byte)1
 #define NO_EEPROM (byte)255
 
 class E2PROM {
   public:
-    static unsigned char read(unsigned char addr) {
+    static byte read(byte addr) {
       EEPROM.begin(EEPROM_BUFFER_SIZE);
-      unsigned char value = EEPROM.read(EEPROM_ADDR_OFFSET + addr);
+      byte value = EEPROM.read(EEPROM_ADDR_OFFSET + addr);
       EEPROM.end();
       return value;
     }
 
-    static void write(unsigned char addr, unsigned char value) {
+    static void write(byte addr, byte value) {
       EEPROM.begin(EEPROM_BUFFER_SIZE);
       EEPROM.write(EEPROM_ADDR_OFFSET + addr, value);
       EEPROM.end();
     }
 
-    static void get(unsigned char addr, float& reference) {
+    static void get(byte addr, float& reference) {
       EEPROM.begin(EEPROM_BUFFER_SIZE);
       EEPROM.get(EEPROM_ADDR_OFFSET + addr, reference);
+      if (isnan(reference)) {
+        reference = 0;
+      }
       EEPROM.end();
     }
 
-    static void put(unsigned char addr, float value) {
+    static void put(byte addr, float value) {
       EEPROM.begin(EEPROM_BUFFER_SIZE);
       EEPROM.put(EEPROM_ADDR_OFFSET + addr, value);
       EEPROM.end();
     }
-    
+
     static void read(String& string) {
       EEPROM.begin(EEPROM_STR_BUFFER_SIZE);
-      for (unsigned char addr = 0; addr < EEPROM_STR_BUFFER_SIZE; ++addr) {
+      for (byte addr = 0; addr < EEPROM_STR_BUFFER_SIZE; ++addr) {
         char c = EEPROM.read(addr);
         if (c == 0) {
           break;
@@ -50,8 +53,8 @@ class E2PROM {
 
     static void write(const String& string) {
       EEPROM.begin(EEPROM_STR_BUFFER_SIZE);
-      unsigned char addr = 0;
-      unsigned char length = min(string.length(), ((unsigned int)EEPROM_STR_BUFFER_SIZE - 1));
+      byte addr = 0;
+      byte length = min(string.length(), ((unsigned int)EEPROM_STR_BUFFER_SIZE - 1));
       for (; addr < length; ++addr) {
         EEPROM.write(addr, string.charAt(addr));
       }
--- src/src.ino
+++ src/src.ino
@@ -1,18 +1,17 @@
 #include <ESP8266WiFi.h>
 
 #include "HERA.h"
-#include "DHT.h"
+#include "E2PROM.h"
 
 //#define __THERMOSTAT__
 //#define __THERMOSTAT_SENSOR__
 //#define __ACTUATOR__
-//#define __ACTION__
 //#define __POWER_METER__
 //#define __NET_SWITCH__
 //#define __COLOR_LED__
-//#define __DHT_SENSOR__
+#define __DHT_SENSOR__
 //#define __LIGHT_DIMMER__
-#define __RADIO_SWITCH__
+//#define __RADIO_SWITCH__
 
 #ifdef __THERMOSTAT__
 const char* HOST_NAME = "thermostat";
@@ -47,7 +46,7 @@ const char* WIFI_PASSWORD = "mami1964";
 
 Device* devices[] = {
 #ifdef __THERMOSTAT__
-  new Thermostat("thermostat", D2, INVERSE, 0.2, 0)
+  new Thermostat("thermostat", D2, INVERSE, 0.1, 0)
 #endif
 
 #ifdef __THERMOSTAT_SENSOR__
@@ -89,60 +88,11 @@ Device* devices[] = {
 #endif
 };
 
-#ifdef __ACTION__
-#include "DeviceAction.h"
-DeviceAction action("thermostat-sensor", "getValue");
-int counter;
-#endif
-
-#ifdef __DHT_SENSOR__
-//DHT sensor(D2, DHT22);
-int readings;
-int errors;
-#endif
-
 void setup() {
   hera.setup(devices, sizeof(devices) / sizeof(Device*));
-
-#ifdef __DHT_SENSOR__
-  readings = 0;
-  errors = 0;
-  //sensor.begin();
-#endif
 }
 
 void loop() {
   hera.loop();
-#ifdef __ACTION__
-  if (action.exec()) {
-    Log::debug(action.value());
-  }
-  Serial.println(counter++);
-#endif
-
-#ifdef __DHT_SENSOR__
-  if (millis() < 4000) {
-    return;
-  }
-
-  /*
-    const String& humidity = devices[0]->invoke("getHumidity");
-    if (humidity.toFloat() == 0.0) {
-      ++errors;
-    }
-    const String& temperature = devices[0]->invoke("getTemperature");
-    if (temperature.toFloat() == 0.0) {
-      ++errors;
-    }
-
-    Serial.print(++readings);
-    Serial.print(" : ");
-    Serial.print(errors);
-    Serial.print(" : ");
-    Serial.print(humidity);
-    Serial.print(" : ");
-    Serial.println(temperature);
-  */
-#endif
 }
 
